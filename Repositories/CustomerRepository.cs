cbc5950393a0744222c723d02269681121bdbb0a
stRafa <rafael.familiamarques2016@gmail.com>
2023-06-30 12:06:11 -0300

    first commit

---
+++ Repositories/CustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using DependencyStore.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using DependencyStore.Repositories.Contracts;
+
+namespace DependencyStore.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly SqlConnection _connection;
+        public CustomerRepository(SqlConnection connection) => _connection = connection;
+        
+        public async Task<Customer?> GetByIdAsync(string customerId)
+        {
+            const string query = "SELECT [Id], [Name], [Email] FROM CUSTOMER WHERE ID=@id";
+            return await _connection.QueryFirstOrDefaultAsync<Customer>(query, new 
+            { 
+                id = customerId 
+            });
+        }
+    }
+}
